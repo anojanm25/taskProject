8d0087cabd1fd939f5e2d7e6dcd503ab4e9e33e2
anojanm25 <125140594+anojanm25@users.noreply.github.com>
2024-04-16 22:36:02 -0400

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 7777;
 
 export const mongoDBURL =
-'mongodb+srv://root:root@bookstore.uooyib5.mongodb.net/?retryWrites=true&w=majority&appName=BookStore';
+'mongodb+srv://root:root@atlascluster.rhx8xyk.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster';
